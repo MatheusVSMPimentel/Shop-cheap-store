1226fd171cfc7f47bdaa81a4310611452a3bd601
Matheus Pimentel <matheusvsm.pimentel@hotmail.com>
2024-08-11 10:39:45 -0300

    fix: wrong backend url, css path

--- shop-cheap-store/src/environments/environment.prod.ts
+++ shop-cheap-store/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrlv1: 'https://localhost:44323/api/v1/',
+  apiUrlv1: 'https://localhost:44321/api/v1/',
   apiImagesUrlv1: 'https://localhost:44321/',
 
 };
